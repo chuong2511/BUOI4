f9fa3ed90512b5ae5f155b629a9f853b239e8ab9
Huynh Hoang Chuong <chuonghufisv1908@gmail.com>
2023-09-06 15:21:24 +0700

    BUOI4

---
+++ BaitapBuoi4/HinhTron.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaitapBuoi4
+{
+    class HinhTron
+    {
+        private ToaDo _tam;
+
+        public ToaDo Tam
+        {
+            get { return _tam; }
+            set { _tam = value; }
+        }
+        private double _banKinh;
+
+        public double BanKinh
+        {
+            get { return _banKinh; }
+            set { _banKinh = value; }
+        }
+
+        public HinhTron()
+        {
+        }
+
+        public HinhTron(ToaDo tam, double banKinh)
+        {
+            this._banKinh = banKinh;
+            this._tam = tam;
+        }
+
+        public double tinhChuVi()
+        {
+            return _banKinh * 2 * 3.14;
+        }
+
+        public double tinhDienTich()
+        {
+            return _banKinh * _banKinh * 3.14;
+        }
+
+        public void nhap()
+        {
+            Console.WriteLine("Nhap tam hinh tron:");
+            _tam = (Console.ReadLine());
+        }
+    }
+}
---
+++ BaitapBuoi4/ToaDo.cs
@@ -0,0 +1,63 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaitapBuoi4
+{
+    class ToaDo
+    {
+        private string _ten;
+
+        public string Ten
+        {
+            get { return _ten; }
+            set { _ten = value; }
+        }
+        private float _x,_y;
+
+        public float Y
+        {
+            get { return _y; }
+            set { _y = value; }
+        }
+
+        public float X
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
+
+        public ToaDo()
+        { 
+        }
+        public ToaDo(string ten, float x, float y)
+        {
+            this._ten = ten;
+            this._x = x;
+            this._y = y;
+        }
+
+        public override string ToString()
+        {
+            return "Ten:" +_ten + "X:" +_x + "Y:" + _y;
+        }
+
+        public void nhap()
+        {
+            Console.WriteLine("Nhap ten toa do:");
+            _ten = Console.ReadLine();
+            Console.WriteLine("Nhap toa do x:");
+            _x = float.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap toa do y:");
+            _y = float.Parse(Console.ReadLine());
+        }
+
+        public void xuat()
+        {
+            Console.WriteLine("Ten toa do: {0}\n Toa do x la: {1}\n Toa do y la: {2}", _ten, _x, _y);
+        }
+    }
+}
